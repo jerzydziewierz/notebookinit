a2ce4f8e6d07f27123834318bf7f54abc198c349
George Rey <1606347+jerzydziewierz@users.noreply.github.com>
2023-05-20 15:36:26 +0100

    Update setup.py
    bump minor version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_version():
     try using `git rev-parse HEAD --short` here to get the git commit.
     then append that to the version string.
     """
-    version = "0.0.1"
+    version = "0.0.3"
     try:
         command = "git rev-parse --verify --short=4 @"
         import os
